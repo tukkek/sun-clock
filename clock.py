10e01f9db76e580ce008d550a05326b97b65db12
Alex Henry <tukkek@gmail.com>
2024-04-26 00:00:00 +0000

    Ugly tooltip implementation

--- clock.py
+++ clock.py
@@ -1,19 +1,36 @@
 #!./venv/bin/python3
-import PyQt5.QtGui,PyQt5.QtWidgets,datetime,PyQt5.QtCore
+import PyQt5.QtGui,PyQt5.QtWidgets,datetime,PyQt5.QtCore,dataclasses
+
+@dataclasses.dataclass
+class Period:
+  name:str
+  hour:int
   
+  def get(hour):
+    while hour>23:
+      hour-=24
+    return next(p for p in periods if hour<p.hour+6)
+  
+periods=[Period('Night',0),Period('Morning',6),Period('Afternoon',12),Period('Evening',18)]  
 a=PyQt5.QtWidgets.QApplication([])
 i=PyQt5.QtWidgets.QSystemTrayIcon()
 text=q=PyQt5.QtWidgets.QAction()
 
-def update():
+def update(hourp=None):
   n=datetime.datetime.now()
+  h=hourp if hourp!=None else n.hour
   m=n.minute
-  h=n.hour
   i.setIcon(PyQt5.QtGui.QIcon(f"icons/{h}.png"))
-  tooltip=f'{h}:{m:02}'
+  p=Period.get(h)
+  later=Period.get(p.hour+6)
+  laterhour=later.hour
+  if laterhour==0:
+    laterhour=24
+  tooltip=f'{h-p.hour} hours past {p.name}'if h-p.hour<3 else f'{laterhour-h} hours to {later.name}'
+  tooltip=f'{tooltip.lower()}, {h}:{m:02}'
+  print(h,tooltip)
   i.setToolTip(tooltip)
   text.setText(tooltip)
-  print(tooltip)
 
 a.setQuitOnLastWindowClosed(False) 
 m=PyQt5.QtWidgets.QMenu() 
@@ -26,6 +43,10 @@ update()
 i.setVisible(True)
 t=PyQt5.QtCore.QTimer()
 t.timeout.connect(update)
-t.setInterval(1*1000)
+t.setInterval(60*1000)
 t.start()
+
+for hour in range(0,24):
+  update(hour)
+
 a.exec() 
