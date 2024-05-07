d15c5b1c8acf4d31d4a093839e5996e2a51d9b31
Alex Henry <tukkek@gmail.com>
2024-05-07 00:00:00 +0000

    Proper measure() of time slices.

--- clock.py
+++ clock.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import PyQt5.QtGui,PyQt5.QtWidgets,datetime,PyQt5.QtCore,dataclasses,sys,webbrowser
+import PyQt5.QtGui,PyQt5.QtWidgets,datetime,PyQt5.QtCore,dataclasses,sys,webbrowser,math
+
+MINUTE=60
+HOUR=MINUTE*60
 
 @dataclasses.dataclass
 class Period:
@@ -9,6 +12,8 @@ class Period:
   def get(hour):
     while hour>23:
       hour-=24
+    while hour<0:
+      hour+=24
     return next(p for p in periods if hour<p.hour+6)
   
 periods=[Period('Night',0),Period('Morning',6),Period('Afternoon',12),Period('Evening',18)]  
@@ -20,27 +25,38 @@ date=q=PyQt5.QtWidgets.QAction()
 def describe(hours):
   return '1 hour' if hours==1 else f'{hours} hours'
 
-def measure(hour):
-  p=Period.get(hour)
-  n=p.name.lower()
-  if hour==p.hour:
-    return p.name
-  if hour-p.hour<3:
-    return f'{describe(hour-p.hour)} past {n}'
-  if hour-3==p.hour:
-    return f'Late {n}'
-  later=Period.get(p.hour+6)
-  laterhour=later.hour
-  if laterhour==0:
-    laterhour=24
-  return f'{describe(laterhour-hour)} to {later.name.lower()}'
-  
+def wind(hour,target,delta,now):
+  d=datetime.datetime(now.year,now.month,now.day,hour=hour)
+  while d.hour!=target.hour:
+    d+=datetime.timedelta(hours=delta)
+  return d
+
+def drop(amount):
+  if amount>10:
+    amount=math.floor(amount/10)*10
+  return math.floor(amount)
+
+def measure(now):
+  h=now.hour
+  earlier=Period.get(h)
+  earlierdelta=now-wind(h,earlier,-1,now)
+  later=Period.get(h+6)
+  laterdelta=wind(h,later,+1,now)-now
+  reference=earlierdelta if earlierdelta<laterdelta else laterdelta
+  hours=math.floor(reference.seconds/HOUR)
+  length=f'{hours} hours' if hours>0 else f'{drop(reference.seconds/MINUTE)} minutes'
+  if length.startswith('1 '):
+    length=length[:-1]
+  if reference==earlierdelta:
+    return f'{length} into {earlier.name.lower()}' 
+  return f'{length} until {later.name.lower()}'
+
 def update():
   path='/'.join(sys.argv[0].split('/')[:-1])
   n=datetime.datetime.now()
   h=n.hour
   i.setIcon(PyQt5.QtGui.QIcon(f"{path}/icons/{h}.png"))
-  tooltip=f'{measure(h)} ({h}:{n.minute:02})'
+  tooltip=f'{measure(n)} ({h}:{n.minute:02})'
   i.setToolTip(tooltip)
   text.setText(tooltip)
   d=n.date()
@@ -50,7 +66,7 @@ a.setQuitOnLastWindowClosed(False)
 m=PyQt5.QtWidgets.QMenu()
 m.addAction(text)
 m.addAction(date)
-m.triggered.connect(lambda:webbrowser.open('https://www.timeanddate.com/calendar/'))
+date.triggered.connect(lambda:webbrowser.open('https://www.timeanddate.com/calendar/'))
 q=PyQt5.QtWidgets.QAction("Quit") 
 q.triggered.connect(a.quit) 
 m.addAction(q) 
