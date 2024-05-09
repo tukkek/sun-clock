44f5b9b3e4651fe74d9dea224176997edeb76ba4
Alex Henry <tukkek@gmail.com>
2024-05-09 00:00:00 +0000

    simple_tray sub-module.

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import PyQt5.QtGui,PyQt5.QtWidgets,datetime,PyQt5.QtCore,dataclasses,sys,webbrowser,math
+import datetime,dataclasses,webbrowser,math,simple_tray.tray,PyQt5.QtWidgets,PyQt5.QtGui
 
 MINUTE=60
 HOUR=MINUTE*60
@@ -17,10 +17,6 @@ class Period:
     return next(p for p in periods if hour<p.hour+6)
   
 periods=[Period('Night',0),Period('Morning',6),Period('Afternoon',12),Period('Evening',18)]  
-a=PyQt5.QtWidgets.QApplication([])
-i=PyQt5.QtWidgets.QSystemTrayIcon()
-text=q=PyQt5.QtWidgets.QAction()
-date=q=PyQt5.QtWidgets.QAction()
 
 def describe(hours):
   return '1 hour' if hours==1 else f'{hours} hours'
@@ -51,31 +47,22 @@ def measure(now):
     return f'{length} into {earlier.name.lower()}' 
   return f'{length} until {later.name.lower()}'
 
-def update():
-  path='/'.join(sys.argv[0].split('/')[:-1])
-  n=datetime.datetime.now()
-  h=n.hour
-  i.setIcon(PyQt5.QtGui.QIcon(f"{path}/icons/{h}.png"))
-  tooltip=f'{measure(n)} ({h}:{n.minute:02})'
-  i.setToolTip(tooltip)
-  text.setText(tooltip)
-  d=n.date()
-  date.setText(f'{d.strftime("%A")}, {d.isoformat()}')
+class Tray(simple_tray.tray.Tray):
+  def update(self):
+    #path='/'.join(sys.argv[0].split('/')[:-1])
+    n=datetime.datetime.now()
+    h=n.hour
+    self.icon.setIcon(PyQt5.QtGui.QIcon(f"icons/{h}.png"))
+    tooltip=f'{measure(n)} ({h}:{n.minute:02})'
+    self.say(tooltip)
+    self.text.setText(tooltip)
+    d=n.date()
+    self.date.setText(f'{d.strftime("%A")}, {d.isoformat()}')
   
-a.setQuitOnLastWindowClosed(False) 
-m=PyQt5.QtWidgets.QMenu()
-i.activated.connect(lambda:m.popup(PyQt5.QtGui.QCursor.pos()))
-m.addAction(text)
-m.addAction(date)
-date.triggered.connect(lambda:webbrowser.open('https://www.timeanddate.com/calendar/'))
-q=PyQt5.QtWidgets.QAction("Quit") 
-q.triggered.connect(a.quit) 
-m.addAction(q) 
-i.setContextMenu(m) 
-update()
-i.setVisible(True)
-t=PyQt5.QtCore.QTimer()
-t.timeout.connect(update)
-t.setInterval(60*1000)
+t=Tray('Sun-clock','',60)
+t.text=q=PyQt5.QtWidgets.QAction()
+t.menu.addAction(t.text)
+t.date=q=PyQt5.QtWidgets.QAction()
+t.date.triggered.connect(lambda:webbrowser.open('https://www.timeanddate.com/calendar/'))
+t.menu.addAction(t.date)
 t.start()
-a.exec() 
